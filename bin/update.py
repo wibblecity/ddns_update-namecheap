47d9e2070726808df06ff52fc2ad6d5ad1eeac82
Robin <git@42x1.com>
2020-04-12 20:48:58 +0100

    Only hit the API when the IP has changed or the forced time limit is reached

--- bin/update.py
+++ bin/update.py
@@ -8,6 +8,11 @@ import socket
 import ConfigParser
 import os.path
 from os import path
+import json
+import hashlib
+import time
+
+results_file = "/tmp/ddns.namecheap.json"
 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -30,21 +35,56 @@ def usage(log_string=''):
     sys.stderr.write('ERROR: ###### ***** ERROR ***** #####' + "\n")
     sys.exit(1)
 
-def update_dns_records(config_file):
+def get_results():
+    if os.path.exists(results_file):
+        with open(results_file) as json_file:
+            results = json.load(json_file)
+    else:
+        results = {}
+    return results
+
+def update_results(node_fqdn,local_ip,ext_ip):
+    node_hash = hashlib.md5(str.encode(node_fqdn)).hexdigest()
+    results = get_results()
+    results[node_hash] = {}
+    results[node_hash]['node_fqdn'] = node_fqdn
+    results[node_hash]['local_ip'] = local_ip
+    results[node_hash]['ext_ip'] = ext_ip
+    results[node_hash]['update_time'] = int(time.time())
+    with open(results_file, 'w') as outfile:
+        json.dump(results, outfile)
+
+def check_dns_records(config_file):
+    results = get_results()
     config = ConfigParser.RawConfigParser()
     config.read(config_file)
     domain_name = config.get('Main', 'domain_name')
     node_id = config.get('Main', 'node_id')
     password = config.get('Main', 'password')
-    update_base_url = "https://dynamicdns.park-your-domain.com/update"
-    node_a_record_int = node_id + "-int"
-    node_a_record_ext = node_id + "-ext"
-    my_local_ip = get_ip()
+    node_fqdn = node_id + '.' + domain_name
+    node_hash = hashlib.md5(str.encode(node_fqdn)).hexdigest()
+    local_ip = get_ip()
     web_page = urllib.urlopen("http://iptools.bizhat.com/ipv4.php")
-    my_ext_ip = web_page.read()
-    update_url = update_base_url + "?host=" + node_a_record_int + "&domain=" + domain_name + "&ip=" + my_local_ip + "&password=" + password
+    ext_ip = web_page.read()
+    if node_hash not in results:
+        update_dns_records(node_id,domain_name,local_ip,ext_ip,password)
+        update_results(node_fqdn,local_ip,ext_ip)
+    elif results[node_hash]['update_time'] < int(time.time()) - (60 * 60 * 24):
+        update_dns_records(node_id,domain_name,local_ip,ext_ip,password)
+        update_results(node_fqdn,local_ip,ext_ip)
+    else:
+        if results[node_hash]['local_ip'] != local_ip or results[node_hash]['ext_ip'] != ext_ip:
+            update_dns_records(node_id,domain_name,local_ip,ext_ip,password)
+            update_results(node_fqdn,local_ip,ext_ip)
+
+def update_dns_records(a_record,domain_name,local_ip,ext_ip,password):
+    print ('Updating DDNS records for: ' + a_record + '.' + domain_name)
+    print ('  Local IP: ' + local_ip)
+    print ('  External IP: ' + ext_ip)
+    update_base_url = "https://dynamicdns.park-your-domain.com/update"
+    update_url = update_base_url + "?host=" + a_record + "-int" + "&domain=" + domain_name + "&ip=" + local_ip + "&password=" + password
     urllib.urlopen(update_url).read()
-    update_url = update_base_url + "?host=" + node_a_record_ext + "&domain=" + domain_name + "&ip=" + my_ext_ip + "&password=" + password
+    update_url = update_base_url + "?host=" + a_record + "-ext" + "&domain=" + domain_name + "&ip=" + ext_ip + "&password=" + password
     urllib.urlopen(update_url).read()
 
 def main(argv):
@@ -59,7 +99,7 @@ def main(argv):
                 usage('--config <arg> - file does not exist: ' + str(arg))
             else:
                 config_file = str(arg)
-    update_dns_records(config_file)
+    check_dns_records(config_file)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
